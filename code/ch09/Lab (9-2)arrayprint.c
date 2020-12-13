a4676338dd9df4e5153579777090d9cd12d5c040
isj003 <71014435+isj003@users.noreply.github.com>
2020-12-13 21:25:10 +0900

    Add files via upload

---
+++ code/ch09/Lab (9-2)arrayprint.c
@@ -0,0 +1,26 @@
+﻿// file: arrayprint.c
+#include <stdio.h>
+
+int main()
+{
+	int a[3][4] = {
+	{1,2,7,3},		/* initialzers for row indexed by 0 */
+	{5,6,3,4},	/* initialzers for row indexed by 1 */
+	{9,7,1,8}		/* initialzers for row indexed by 2 */
+	};
+	printf("%6s %6s %3s", "원소", "주소", "값");
+	printf("%6s %6s %3s", "원소", "주소", "값");
+	printf("%6s %6s %3s", "원소", "주소", "값");
+	printf("%6s %6s %3s\n", "원소", "주소", "값");
+	printf("-----------------------------------------");
+	printf("----------------------------------------\n");
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 4; j++)
+			printf(" a[%d] [%d] %d %d", i, j, &a[i][j], a[i][j]);
+		puts("");
+	}
+
+	return 0;
+}
