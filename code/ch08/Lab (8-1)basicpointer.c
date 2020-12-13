65cf1363abbc3fc4e2daaa8c3251fae5576047e0
isj003 <71014435+isj003@users.noreply.github.com>
2020-12-13 22:54:20 +0900

    Update Lab (8-1)basicpointer.c

--- code/ch08/Lab (8-1)basicpointer.c
+++ code/ch08/Lab (8-1)basicpointer.c
@@ -1,23 +1,20 @@
-﻿// file: variousop.c
+// file : basicpointer.c
 #include <stdio.h>
 
 int main(void)
 {
-	int i;
-	int* pi = &i;	//포인트 선언
-	int** dpi = &pi;	//이중포인터 선언
+	char c = '@';
+	char* pc = &c;
+	int m = 100;
+	int* pm = &m;
+	double x = 5.83;
+	double* px = &x;
 
-	*pi = 5;
-	*pi += 1;
-	printf("%d\n", i);
-	// 후위 연산자 pi++는 전위 연산자보다 *pi보다 빠름
-	printf("%d\n", (*pi)++);	//*pi++ 는 *(pi**)로 (*pi)++와 다름
-	printf("%d\n", *pi);
-
-	*pi = 10;
-	printf("%d\n", ++ * pi);	//***pi와 ++(*pi)는 같음
-	printf("%d\n", ++ * *dpi);	//++**dpi와 ++(**dpi)는 같음
-	printf("%d\n", i);
+	printf("변수명	주소값	저장값\n");
+	printf("--------------------------------------\n");
+	printf("%3s %12p %9c\n", "c", pc, c);
+	printf("%3s %12p %9d\n", "m", pm, m);
+	printf("%3s %12p %9f\n", "x", px, x);
 
 	return 0;
 }
