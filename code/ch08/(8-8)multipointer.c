525cdd49c491ae1d94527a0dec2418db04af3241
isj003 <71014435+isj003@users.noreply.github.com>
2020-12-13 21:22:11 +0900

    Add files via upload

---
+++ code/ch08/(8-8)multipointer.c
@@ -0,0 +1,20 @@
+﻿// file: multipointer.c
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 20;
+	int* pi = &i;
+	int** dpi = &pi;
+
+	printf("%p %p %p\n", &i, pi, *dpi);
+
+	*pi = i + 2;	// i = i + 2;
+	printf("%d %d %d\n", i, *pi, **dpi);
+
+	**dpi = *pi + 2;	// i = i + 2;
+	printf("%d %d %d\n", i, *pi, **dpi);
+
+	return 0;
+}
+
